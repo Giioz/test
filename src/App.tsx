08a8e1f05c293119ad503d8e809a955d589c3049
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-11-18 16:26:46 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import JumpScare from './JumpScare'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <JumpScare imagePath="./images.jpeg" soundPath="./Jumpscare Sound Effect.mp3"/>
